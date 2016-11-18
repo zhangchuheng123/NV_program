70b70ca0a6b1e56152ece6c4494d24e15592babd
Zhang Chuheng <zhangchuheng123@live.com>
2016-11-18 08:16:39 +0800

    update

--- NVCoordinate.m
+++ NVCoordinate.m
@@ -1,13 +1,13 @@
 %% parameter area
 % the position of four markers
 % sequence: (0,0) (0,30) (30,0) (30,30)
-F(:,1)=[29.2; 30];
-F(:,2)=[29.6; 60];
-F(:,3)=[59; 29.6];
-F(:,4)=[59.4, 59.4];
+F(:,1)=[58.4; 59.0];
+F(:,2)=[88.5; 58.5];
+F(:,3)=[58.7; 88.7];
+F(:,4)=[88.6; 88.5];
 % (26.5, 64)  (25.5, 93.5) (56.5, 64.5) (55.5, 94.5)
 % NV position
-NVposition=[49.8; 74.6];
+NVposition=[86.7; 65.4];
 
 %% Program area
 fprintf('Four markers: (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) \n', ...
@@ -25,8 +25,8 @@ Fx=F(:,4)-F(:,1);
 AB=Fab\Fx;
 Sd=AB(1,1)*Sa+AB(2,1)*Sb;
 deltaxy=Sd-S(:,4);
-fprintf('deltaxy = (%.1f, %.1f) \n', deltaxy(1), deltaxy(2));
+fprintf('deltaxy = (%.4f, %.4f) \n', deltaxy(1), deltaxy(2));
 
 tm=Fab\(NVposition-F(:,1));
 NVture=tm(1,1)*Sa+tm(2,1)*Sb;
-fprintf('NVture = (%.1f, %.1f) \n', NVture(1), NVture(2));
+fprintf('NVture = (%.4f, %.4f) \n', NVture(1), NVture(2));
