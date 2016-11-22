4dd4019a72e5cd8c639d564797e672cd7f187973
Zhang Chuheng <zhangchuheng123@live.com>
2016-11-22 15:36:53 +0800

    auto small marker

--- findNV.m
+++ findNV.m
@@ -1,5 +1,5 @@
 %% move it to a position
-identifier = 'Scan_natural-4_';
+identifier = 'Scan_natural-3_';
 scan(10, 10, 20, 1, 0, identifier);
 
 %% scanz example
@@ -10,7 +10,7 @@ CountNum = 40;
 scan(X, Y, Z, CountNum, 0, identifier);
 
 %% sample big marker
-Z0 = 26.5;
+Z0 = 24.5;
 X = 1:0.5:20;
 Y = 1:0.5:20;
 Z = 0;
@@ -19,6 +19,9 @@ CountNum = 1;
 scan(X, Y, Z, CountNum, Z0, identifier);
 
 %% sample area
+cross_x = 6.0;
+cross_y = 10.5;
+
 X = 20:0.5:90;
 Y = 20:0.5:90;
 Z = [6, 7, 8];
@@ -27,8 +30,8 @@ CountNum = 1;
 scan(X, Y, Z, CountNum, Z0, identifier);
 
 %% Center scan for NV
-X0 = 84.5;
-Y0 = 52.5;
+X0 = 54;
+Y0 = 36;
 Delta = 3;
 X = X0-Delta:0.1:X0+Delta;
 Y = Y0-Delta:0.1:Y0+Delta;
@@ -38,11 +41,11 @@ CountNum = 1;
 scan(X, Y, Z, CountNum, Z0, identifier);
 
 %% scanz over NV
-X0 = 80.7;
-Y0 = 57.3;
+NV_x = 54.0;
+NV_y = 35.7;
 Z = 20:0.1:35;
 CountNum = 40;
-scan(X0, Y0, Z, CountNum, 0, identifier);
+scan(NV_x, NV_y, Z, CountNum, 0, identifier);
 
 %% Coarse scan for small markers
 X = 25:0.5:65;
@@ -53,8 +56,22 @@ CountNum = 1;
 scan(X, Y, Z, CountNum, Z0, identifier);
 
 %% Scan for small marker
-X0 = 29.5;
-Y0 = 60.0;
+first_x = cross_x + 18.5;
+first_y = cross_y + 23.5;
+
+if ((NV_x < first_x + 30) && (NV_y < first_y + 30))
+    X0 = first_x;
+    Y0 = first_y;
+elseif ((NV_x < first_x + 60) && (NV_y < first_y + 30))
+    X0 = first_x+30;
+    Y0 = first_y;
+elseif ((NV_x < first_x + 30) && (NV_y < first_y + 60))
+    X0 = first_x;
+    Y0 = first_y+30;
+else
+    X0 = first_x+30;
+    Y0 = first_y+30;
+end
 Delta = 3;
 
 X = X0-Delta:0.1:X0+Delta;
