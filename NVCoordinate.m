636d1ac6622362497694f97b563b1736c0d5c61d
Zhang Chuheng <zhangchuheng123@live.com>
2016-11-26 09:34:58 +0800

    update

--- NVCoordinate.m
+++ NVCoordinate.m
@@ -17,12 +17,12 @@
 %  F1+----------+F2   :---> x
 
 % sequence: (0,0) (0,30) (30,0) (30,30)
-F(:,1)=[29.9 62.4]';
-F(:,2)=[59.8 61.9]';
-F(:,3)=[30.2 92.2]';
-F(:,4)=[60.1 91.7]';
+F(:,1)=[58.2 31.5]';
+F(:,2)=[87.9 31.1]';
+F(:,3)=[58.5 61.4]';
+F(:,4)=[88.3 60.9]';
 % NV position
-NVposition=[23.9 70.1]';
+NVposition=[62.0, 23.7]';
 
 %% Program area
 % standard marker relative position
@@ -33,7 +33,7 @@ F = [F(:,3), F(:,1), F(:,4), F(:,2)];
 S = [S(:,3), S(:,1), S(:,4), S(:,2)];
 
 % output
-fprintf('\nFour markers (in UV order): (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) \n', ...
+fprintf('\nFour markers (XY in UV order): (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) \n', ...
     F(1,1), F(2,1), F(1,2), F(2,2), F(1,3), F(2,3), F(1,4), F(2,4));
 %error from XY axis coordinate
 Fa=F(:,2)-F(:,1);
@@ -46,8 +46,8 @@ Fx=F(:,4)-F(:,1);
 AB=Fab\Fx;
 Sd=AB(1)*Sa+AB(2)*Sb;
 deltaxy=Sd-(S(:,4)-S(:,1));
-fprintf('deltaxy (in UV direction) = (%.4f, %.4f) \n', deltaxy(1), deltaxy(2));
+fprintf('deltaxy (XY) = (%.3f, %.3f) \n', deltaxy(1), deltaxy(2));
 
 tm=Fab\(NVposition-F(:,1));
 NVture=tm(1)*Sa+tm(2)*Sb;
-fprintf('NVture (in UV direction) = (%.4f, %.4f) \n', -NVture(2), NVture(1));
+fprintf('NVture (in UV direction) = (%.3f, %.3f) \n', -NVture(2), NVture(1));
