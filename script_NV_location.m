b854bed600250f1519c41355df5ba1a652f0ac55
Zhang Chuheng <zhangchuheng123@live.com>
2017-02-11 14:55:24 +0800

    update

--- script_NV_location.m
+++ script_NV_location.m
@@ -1,6 +1,6 @@
 %% move it to a position
 global parameters;
-parameters = default_parameter_constructor('lab5-lian');
+parameters = default_parameter_constructor('lab1-jiang');
 parameters.figure.identifier =  'Scan_natural-3_';
 tools = experiment_toolbox;
 tools.scan(10, 10, 20, 1, 0);
