cce5cc8f735993dbba8abe1faac85d5651ce1ca5
Zhang Chuheng <zhangchuheng123@live.com>
2017-03-02 10:02:29 +0800

    update

--- script_large_scan.m
+++ script_large_scan.m
@@ -28,40 +28,40 @@ tools.scan(50, 50, Z0, 0);
 tools.large_scan(X_vol, Y_vol, APT_X, APT_Y, Z, CountNum, Z0);
 
 %% density scan mirror
-Z0 = 56;
-X_vol = -200:2:200;
-Y_vol = -200:2:200;
-Z_rel = [0, 4, 6, 10];
-% Z_rel = 30;
+Z0 = 54.5;
+X_vol = -100:2:100;
+Y_vol = -100:2:100;
+% Z_rel = [0, 4, 6, 10];
+Z_rel = 5;
 Z = Z0 + Z_rel;
 CountNum = 1;
-tools.scan(10, 10, Z0, 0);
+tools.scan(50, 50, Z0, 0);
 tools.scan_mirror_fast(X_vol, Y_vol, Z, CountNum, Z0);
 
 %% density scan
-Z0 = 56;
+Z0 = 54.5;
 X = 20:0.3:80;
 Y = 20:0.3:80;
 % Z_rel = [6, 8, 10, 15, 20, 25, 30];
-Z_rel = 8;
+Z_rel = 5;
 Z = Z0 + Z_rel;
 CountNum = 1;
-tools.scan_mirror_fast(-200, 0, 0, 1);
+tools.scan_mirror_fast(-200, -200, 0, 1);
 tools.scan_fast(X, Y, Z, CountNum, Z0);
 
 %% center scan
-X0 = 120;
-Y0 = 14;
+X0 = -25;
+Y0 = -93;
 delta = 5;
 X_vol = X0-delta:1:X0+delta;
 Y_vol = Y0-delta:1:Y0+delta;
 CountNum = 10;
-tools.scan(50, 50, Z0, 0);
+% tools.scan(50, 50, Z0, 0);
 tools.scan_mirror_fast(X_vol, Y_vol, Z, CountNum, Z0);
 
 %% center scan
-X0 = 50;
-Y0 = 50;
+X0 = 47.6;
+Y0 = 57.8;
 delta = 1;
 X = X0-delta:0.1:X0+delta;
 Y = Y0-delta:0.1:Y0+delta;
@@ -69,9 +69,9 @@ CountNum  = 20;
 tools.scan_fast(X, Y, Z, CountNum, Z0);
 
 %%
-X0 = 50.1;
+X0 = 50;
 Y0 = 50;
-Z = 35.5;
+Z = 54.5;
 Z = Z-10:0.2:Z+10;
 CountNum = 10;
 tools.scan(X0, Y0, Z, CountNum, Z0);
