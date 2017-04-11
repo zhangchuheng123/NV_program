aa7add574555eb3f5ba9ccfba3cd8e96983852c5
Zhang Chuheng <zhangchuheng123@live.com>
2017-04-11 08:09:43 +0800

    update NV_coordinate
    You can input F in any order

--- script_NV_coordinate.m
+++ script_NV_coordinate.m
@@ -1,29 +1,23 @@
 function script_NV_coordinate(F, NVposition)
-	% the position of four markers
-	% in matlab view            in UV
-	%  F1+----------+F2         F4+----------+F2
-	%    |          |             |          |
-	%    |          |             |          |
-	%    |          |   --->      |          |
-	%    |          |             |          |
-	%  F3+----------+F4         F3+----------+F1
-	%
-	% in UV order
-	%  F3+----------+F4
-	%    |          |
-	%    |          |     ^ y
-	%    |          |     |
-	%    |          |     |
-	%  F1+----------+F2   :---> x
-
-	% Program area
 	% standard marker relative position
-	S=[0 30 0 30; 0 0 30 30];
-
-	F = [F(:,3), F(:,1), F(:,4), F(:,2)];
-	S = [S(:,3), S(:,1), S(:,4), S(:,2)];
-
-	fprintf('\nFour markers (XY in UV order): (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) \n', ...
+	S=[
+        0 30 0 30; 
+        0 0 30 30
+        ];
+    
+    % adjust F to standard order
+    F_adjust = zeros(2, 4);
+    F = [F; sum(F,1)];
+    F_adjust(:, 1) = F(1:2, F(3,:) == min(F(3,:)));
+    F(1:2, F(3,:) == min(F(3,:))) = [0; 0];
+    F_adjust(:, 4) = F(1:2, F(3,:) == max(F(3,:)));
+    F(1:2, F(3,:) == max(F(3,:))) = [0; 0];
+    F_adjust(:, 2) = F(1:2, F(1,:) == max(F(1,:)));
+    F(1:2, F(1,:) == max(F(1,:))) = [0; 0];
+    F_adjust(:, 3) = F(1:2, F(2,:) == max(F(2,:)));
+    
+    F = F_adjust;
+	fprintf('\nFour markers: (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) (%.1f, %.1f) \n', ...
 	    F(1,1), F(2,1), F(1,2), F(2,2), F(1,3), F(2,3), F(1,4), F(2,4));
 	Fa=F(:,2)-F(:,1);
 	Fb=F(:,3)-F(:,1);
@@ -39,5 +33,5 @@ function script_NV_coordinate(F, NVposition)
 
 	tm=Fab\(NVposition - F(:,1));
 	NVture=tm(1)*Sa+tm(2)*Sb;
-	fprintf('NVture (in UV direction) = (%.3f, %.3f) \n', -NVture(2), NVture(1));
+	fprintf('NVture (in UV direction) = (%.3f, %.3f) \n', NVture(1), NVture(2));
 end
